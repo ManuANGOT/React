a223e146c3e6132639b38070230a8d9d92cd3edd
ManuAngot <manu.angot@hotmail.fr>
2022-12-06 13:32:33 +0100

    composant

---
+++ composant/src/composant_coq.md
@@ -0,0 +1,8 @@
+## Création d'un composant
+
+Crée dans app un composant Réact qui contiendra 5 contacts.
+
+Chaques contact contiendra un nom, un prénom, un numéro de téléphone et un email.
+
+et un boutton supprimer.
+
---
+++ coursreact/src/App.js
@@ -0,0 +1,14 @@
+import { Fragment } from "react";
+
+function App() {
+  return (
+    <Fragment>
+      <div>
+        <h1>"Hello World !!!"</h1>
+        <h2> Toto </h2>
+      </div>
+    </Fragment>
+  );
+}
+
+export default App;
