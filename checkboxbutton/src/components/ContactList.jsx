2a9f8243e6fd4c2164bc151684d2382c07f835c0
ManuANGOT <manu.angot@hotmail.fr>
2022-12-07 14:37:50 +0100

    ajout des packs json

--- checkboxbutton/src/components/ContactList.jsx
+++ checkboxbutton/src/components/ContactList.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import {Link} from "react-router-dom";
 import Contact from './Contact/Contact';
 
+// Importation de fichiers comportant les données.
 import personnes from"../data";
 
+// Création et exportation du composant ContactList
 export default function ContactList(){
     return (
         <>
         <Link to={"/Count"}>count</Link>
 
+{/* Méthode map sure le fichier data pour avoir une liste de contacts */}
         {personnes.map((personne, index) => (
         <Contact key={index} {...personne} />
       ))}
